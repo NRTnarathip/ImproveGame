c757f1332c204c654cb71857ffebd303105327d9
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-11-21 14:09:10 +0700

    Update

--- PostBuild.cmd
+++ PostBuild.cmd
@@ -1,15 +1,14 @@
-echo my current folder excute %cd%
-adb shell am force-stop com.nrt.fakestardewgame
+set AppName=abc.smapi.gameloader
+adb shell am force-stop %AppName%
 
 if errorlevel 1 (
     echo ADB is not connected.
     exit /b
 )
 
- adb shell mkdir  "/storage/emulated/0/Android/data/com.nrt.fakestardewgame/files/Mods/ImproveGame"
- adb push "bin/ARM64/Release/net8.0/ImproveGame.dll" "/storage/emulated/0/Android/data/com.nrt.fakestardewgame/files/Mods/ImproveGame"
- adb push "bin/ARM64/Release/net8.0/manifest.json" "/storage/emulated/0/Android/data/com.nrt.fakestardewgame/files/Mods/ImproveGame"
+ adb shell mkdir  "/storage/emulated/0/Android/data/%AppName%/files/Mods/ImproveGame"
+ adb push "bin/ARM64/Release/net8.0/ImproveGame.dll" "/storage/emulated/0/Android/data/%AppName%/files/Mods/ImproveGame"
+ adb push "bin/ARM64/Release/net8.0/manifest.json" "/storage/emulated/0/Android/data/%AppName%/files/Mods/ImproveGame"
 
 
- adb shell am start com.nrt.fakestardewgame/crc641b6952874c843248.SMainActivity
-
+adb shell am start %AppName%"/crc644389b739a03c2b33.EntryActivity"
