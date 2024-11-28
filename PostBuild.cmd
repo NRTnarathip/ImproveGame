c723286923a4c673370ff7ff6a1350d74e2df84a
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2024-11-28 20:41:56 +0700

    Update: 2.0.0 For 1.6

--- PostBuild.cmd
+++ PostBuild.cmd
@@ -10,5 +10,3 @@ if errorlevel 1 (
  adb push "bin/ARM64/Release/net8.0/ImproveGame.dll" "/storage/emulated/0/Android/data/%AppName%/files/Mods/ImproveGame"
  adb push "bin/ARM64/Release/net8.0/manifest.json" "/storage/emulated/0/Android/data/%AppName%/files/Mods/ImproveGame"
 
-
-adb shell am start %AppName%"/crc644389b739a03c2b33.EntryActivity"
